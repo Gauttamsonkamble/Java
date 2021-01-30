c772e35a37d0daff4280fae485b27b46c42fb471
Gauttam Sonkamble <46339795+Gauttamsonkamble@users.noreply.github.com>
2021-01-30 11:59:36 +0530

    Add files via upload

---
+++ Collection Framework/ArrayList_Java/src/com/Gauttam/ArrayList_Programs.java
@@ -0,0 +1,22 @@
+package com.Gauttam;
+
+import java.util.ArrayList;
+
+public class ArrayList_Programs {
+
+	public static void main(String[] args) {
+		
+		ArrayList list = new ArrayList();
+		
+			list.add(1);
+			list.add(2);
+			list.add(3);
+			list.add(4);
+			list.add(5);
+			list.add(6);
+			
+			System.out.println(list);
+
+	}
+
+}
