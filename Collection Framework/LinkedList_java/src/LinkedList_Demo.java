5a36b487bdfde4d63954c9c752d087b1d5ef019c
Gauttam Sonkamble <46339795+Gauttamsonkamble@users.noreply.github.com>
2021-03-28 09:58:01 +0530

    Add files via upload

---
+++ Collection Framework/LinkedList_java/src/LinkedList_Demo.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class LinkedList_Demo {
+
+	public static void main(String[] args) {
+		
+		LinkedList list = new LinkedList();
+		
+		list.add(10);
+		list.add(20);
+		list.add(30);
+		list.add(40);
+		list.add(50);
+		
+		System.out.println(list);
+	}
+
+}
