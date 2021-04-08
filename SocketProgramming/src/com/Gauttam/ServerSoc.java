3d9ad5d5f9a943cd973334c5d30871fde339661a
Gauttam Sonkamble <46339795+Gauttamsonkamble@users.noreply.github.com>
2021-04-09 01:12:51 +0530

    Add files via upload

---
+++ SocketProgramming/src/com/Gauttam/ServerSoc.java
@@ -0,0 +1,33 @@
+package com.Gauttam;
+
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServerSoc {
+
+	public static void main(String[] args) throws IOException {
+		
+		
+		System.out.println("Server is waiting to client");
+		ServerSocket ss = new ServerSocket(9001);
+		
+		Socket s = ss.accept();
+		
+		System.out.println("Connection Established");
+		
+		
+		DataInputStream dis = new DataInputStream(s.getInputStream());
+		
+		String str = dis.readUTF();
+		
+		System.out.println("Message :"+str);
+		
+		ss.close();
+		
+		
+		
+	}
+
+}
